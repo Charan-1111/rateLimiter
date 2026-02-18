15dcff479e60de38cfe5a70259501a16ed194388
Charan <ravivijayacharan0@gmail.com>
2026-02-18 08:47:07 +0530

    changes to the add token function

--- algorithms/tokenBucket.go
+++ algorithms/tokenBucket.go
@@ -1,17 +1,34 @@
 package algorithms
 
+import "fmt"
+
 type TokenBucket struct {
-	maxTokens int
-	tokens []string
+	maxTokens       int
+	tokensAvailable []string
+	tokensQueue     []string
 }
 
 func NewTokenBucket(maxTokens int) *TokenBucket {
 	return &TokenBucket{
-		maxTokens: maxTokens,
-		tokens: make([]string, maxTokens),
+		maxTokens:       maxTokens,
+		tokensAvailable: make([]string, maxTokens),
+		tokensQueue:     make([]string, 0),
 	}
 }
 
 func (tb *TokenBucket) AddTokens() {
-	
+	// this function is going to run periodically
+	if tb.IsBucketFull() {
+		fmt.Println("Bucket is full cannot add tokens")
+		return
+	}
+
+}
+
+func (tb *TokenBucket) IsBucketFull() bool {
+	return len(tb.tokensAvailable) == tb.maxTokens
+}
+
+func (tb *TokenBucket) IsBucketEmpty() bool {
+	return len(tb.tokensAvailable) == 0
 }
