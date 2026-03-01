409df107d04b857202530eacdae4875f5a592d15
Leela Guru Charan Avvaru <78796571+Charan-1111@users.noreply.github.com>
2026-03-01 20:26:57 +0530

    Update tokenBucket.go

--- algorithms/tokenBucket.go
+++ algorithms/tokenBucket.go
@@ -4,10 +4,10 @@ import (
 	"context"
 	"fmt"
 	"goapp/constants"
+	"goapp/lua"
 	"goapp/store"
 	"time"
 
-	"github.com/bytedance/sonic"
 	"github.com/redis/go-redis/v9"
 )
 
@@ -33,57 +33,76 @@ func NewTokenBucket(maxTokens, refillRate float64) *TokenBucket {
 }
 
 func (tb *TokenBucket) Allow(ctx context.Context, tenantId, userId string) (bool, error) {
-	tokens := &Tokens{}
+	// tokens := &Tokens{}
 
 	// get the information from the redis for the key
 	redisKey := fmt.Sprintf("%s:%s:%s:%s", constants.KeyRateLimit, constants.AlgorithmTokenBucket, tenantId, userId)
 
-	val, err := store.Rdb.Get(ctx, redisKey).Result()
-	if err == redis.Nil {
-		fmt.Println("redis is null")
-		tokens.Tokens = tb.MaxTokens
-		tokens.LastRefill = time.Now()
-	} else if err != nil {
-		fmt.Println("error getting the key from redis", err)
-		return false, err
-	} else {
-		// unmarshal the value
-		err = sonic.Unmarshal([]byte(val), &tokens)
-		if err != nil {
-			fmt.Println("error unmarshalling the value", err)
-			return false, err
-		}
-	}
+	tokenBucketScript := redis.NewScript(lua.GetTokenBucketScript())
 
-	// refill the tokens for this key
-	tokens.Tokens = tokens.Tokens + (time.Since(tokens.LastRefill).Minutes() * tb.RefillRate)
-	if tokens.Tokens >= tb.MaxTokens {
-		tokens.Tokens = tb.MaxTokens
-	}
-	tokens.LastRefill = time.Now()
+	// val, err := store.Rdb.Get(ctx, redisKey).Result()
+	// if err == redis.Nil {
+	// 	fmt.Println("redis is null")
+	// 	tokens.Tokens = tb.MaxTokens
+	// 	tokens.LastRefill = time.Now()
+	// } else if err != nil {
+	// 	fmt.Println("error getting the key from redis", err)
+	// 	return false, err
+	// } else {
+	// 	// unmarshal the value
+	// 	err = sonic.Unmarshal([]byte(val), &tokens)
+	// 	if err != nil {
+	// 		fmt.Println("error unmarshalling the value", err)
+	// 		return false, err
+	// 	}
+	// }
 
-	fmt.Println("tokens : ", tokens)
+	// // refill the tokens for this key
+	// tokens.Tokens = tokens.Tokens + (time.Since(tokens.LastRefill).Minutes() * tb.RefillRate)
+	// if tokens.Tokens >= tb.MaxTokens {
+	// 	tokens.Tokens = tb.MaxTokens
+	// }
+	// tokens.LastRefill = time.Now()
 
-	if tokens.Tokens < 1 {
-		return false, fmt.Errorf("bucket is empty, request is getting denied")
-	}
+	// fmt.Println("tokens : ", tokens)
 
-	fmt.Println("token available, request is getting proceed")
+	// if tokens.Tokens < 1 {
+	// 	return false, fmt.Errorf("bucket is empty, request is getting denied")
+	// }
 
-	tokens.Tokens = tokens.Tokens - 1
+	// fmt.Println("token available, request is getting proceed")
 
-	// Set the information in the redis
-	fmt.Println("tokens : ", tokens)
+	// tokens.Tokens = tokens.Tokens - 1
 
-	marshaledVal, err := sonic.Marshal(tokens)
+	// // Set the information in the redis
+	// fmt.Println("tokens : ", tokens)
+
+	// marshaledVal, err := sonic.Marshal(tokens)
+	// if err != nil {
+	// 	fmt.Println("Error marshaling the error : ", err)
+	// 	return false, err
+	// }
+
+	// err = store.Rdb.Set(ctx, redisKey, marshaledVal, time.Second*60).Err()
+	// if err != nil {
+	// 	fmt.Println("error setting the key in redis", err)
+	// 	return false, err
+	// }
+
+	now := float64(time.Now().UnixNano()) / 1e9
+
+	_, err := tokenBucketScript.Run(
+		ctx,
+		store.Rdb,
+		[]string{redisKey},
+		tb.MaxTokens,
+		tb.RefillRate,
+		now,
+		1,
+	).Result()
+
 	if err != nil {
-		fmt.Println("Error marshaling the error : ", err)
-		return false, err
-	}
-
-	err = store.Rdb.Set(ctx, redisKey, marshaledVal, time.Second*60).Err()
-	if err != nil {
-		fmt.Println("error setting the key in redis", err)
+		fmt.Println("Error running the token bucket script : ", err)
 		return false, err
 	}
 
