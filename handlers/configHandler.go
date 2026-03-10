f1df149d6019234b0bf346cf268d5841acb83cb0
Leela Guru Charan Avvaru <78796571+Charan-1111@users.noreply.github.com>
2026-03-10 10:13:45 +0530

    Update configHandler.go

--- handlers/configHandler.go
+++ handlers/configHandler.go
@@ -4,18 +4,24 @@ import (
 	"goapp/algorithms"
 	"goapp/utils"
 
+	"github.com/jackc/pgx/v5/pgxpool"
 	"github.com/redis/go-redis/v9"
+	"github.com/rs/zerolog"
 )
 
 type ConfigHandler struct {
 	config  *utils.Config
+	log     zerolog.Logger
+	db      *pgxpool.Pool
 	rdb     *redis.Client
 	factory algorithms.LimiterFactory
 }
 
-func NewConfigHandler(config *utils.Config, rdb *redis.Client, factory algorithms.LimiterFactory) *ConfigHandler {
+func NewConfigHandler(config *utils.Config, log zerolog.Logger, db *pgxpool.Pool, rdb *redis.Client, factory algorithms.LimiterFactory) *ConfigHandler {
 	return &ConfigHandler{
 		config:  config,
+		log:     log,
+		db:      db,
 		rdb:     rdb,
 		factory: factory,
 	}
