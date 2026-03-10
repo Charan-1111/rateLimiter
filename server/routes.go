aad6571fdd8fb8d87903ef87721a1d9df81e4f22
Leela Guru Charan Avvaru <78796571+Charan-1111@users.noreply.github.com>
2026-03-10 10:13:30 +0530

    Update routes.go

--- server/routes.go
+++ server/routes.go
@@ -9,7 +9,7 @@ import (
 func (app *Application) SetupRoutes() *fiber.App {
 	appServer := fiber.New()
 
-	configHandler := handlers.NewConfigHandler(app.config, app.rdb, app.factory)
+	configHandler := handlers.NewConfigHandler(app.config, app.log, app.db, app.rdb, app.factory)
 
 	// Defining the routes
 	appServer.Get("/api/v1/limiter", configHandler.GetLimiter)
