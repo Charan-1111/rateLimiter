40a770a0dbb8caa512d469176298e3a985257bb3
Leela Guru Charan Avvaru <78796571+Charan-1111@users.noreply.github.com>
2026-03-10 10:13:16 +0530

    Update application.go

--- server/application.go
+++ server/application.go
@@ -1,41 +1,59 @@
 package server
 
 import (
+	"context"
 	"fmt"
 	"goapp/algorithms"
+	"goapp/logger"
 	"goapp/store"
 	"goapp/utils"
 
+	"github.com/jackc/pgx/v5/pgxpool"
 	"github.com/redis/go-redis/v9"
+	"github.com/rs/zerolog"
 )
 
 type Application struct {
 	config  *utils.Config
+	log     zerolog.Logger
+	db      *pgxpool.Pool
 	rdb     *redis.Client
 	factory algorithms.LimiterFactory
 }
 
 func NewApplication(filePath string) (*Application, error) {
+	// Initiating the log
+	log := logger.InitLogger()
+
 	// Load the configuration file
 	config := &utils.Config{}
 	if err := config.LoadConfig(filePath); err != nil {
+		log.Error().Err(err).Msg("Error loading the config file")
 		return nil, err
 	}
 
+	// Initialize the database
+
+	db, _ := config.Database.InitDb(context.Background(), log)
+
 	// Initialize Redis
-	rdb := store.InitRedis(&config.Redis)
+	rdb := store.InitRedis(&config.Redis, log)
 
 	// creating the defautl limiter factory
 	factory := &algorithms.DefaultLimiterFactory{}
 
 	return &Application{
 		config:  config,
+		log:     log,
+		db:      db,
 		rdb:     rdb,
 		factory: factory,
 	}, nil
 }
 
 func (app *Application) StartServer() error {
+	// create the tables
+	store.CreateTables(context.Background(), app.db, app.log, app.config.Tables)
 	// Start fiber server
 	app.StartFiberServer()
 
