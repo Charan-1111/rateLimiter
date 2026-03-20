b4617dc03172631aafd50a5498b8479c0de36466
Leela Guru Charan Avvaru <78796571+Charan-1111@users.noreply.github.com>
2026-03-20 11:58:10 +0530

    Update leakyBucket.go

--- algorithms/leakyBucket.go
+++ algorithms/leakyBucket.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"goapp/constants"
 	"goapp/lua"
+	"goapp/models"
 	"goapp/services"
 	"goapp/utils"
 	"sync"
@@ -31,7 +32,7 @@ func NewLeakyBucket(maxTokens, leakRate float64) *LeakyBucketRedis {
 	}
 }
 
-func (lb *LeakyBucketRedis) Allow(ctx context.Context, rdb *redis.Client, cb *services.CircuitBreaker, log zerolog.Logger, scope, identifier string) (bool, error) {
+func (lb *LeakyBucketRedis) Allow(ctx context.Context, rdb *redis.Client, cb *services.CircuitBreaker, log zerolog.Logger, scope, identifier string) (*models.LimiterResponse, error) {
 	// read data from redis
 	redisKey := utils.StringBuilder(constants.KeyRateLimit, constants.AlgorithmLeakyBucket, scope, identifier)
 
@@ -39,24 +40,30 @@ func (lb *LeakyBucketRedis) Allow(ctx context.Context, rdb *redis.Client, cb *se
 
 	now := float64(time.Now().UnixNano()) / 1e9
 
-	// _, err := leakyScript.Run(ctx, rdb, []string{redisKey}, lb.MaxTokens, lb.LeakRate, now, 1).Result()
-	// if err != nil {
-	// 	fmt.Println("Error running the script, rejecting the request : ", err)
-	// 	return false, err
-	// } else {
-	// 	fmt.Println("request accepted")
-	// }
-
-	_, err := cb.Cb.Execute(func() (any, error) {
-		return leakyScript.Run(ctx, rdb, []string{redisKey}, lb.MaxTokens, lb.LeakRate, now, 1).Result()
+	results, err := cb.Cb.Execute(func() (any, error) {
+		results, err := leakyScript.Run(ctx, rdb, []string{redisKey}, lb.MaxTokens, lb.LeakRate, now, 1).Result()
+		return results, err
 	})
+
+	allowed := results.([]any)[0].(bool)
+	currentTokens := results.([]any)[1].(float64)
+
+	retryAfter := now + (lb.MaxTokens / lb.LeakRate)
 
 	if err != nil {
 		log.Error().Err(err).Msg("Error running the script, rejecting the request")
-		return false, err
+		return &models.LimiterResponse{
+			Allowed:       false,
+			RetryAfter:    0,
+			CurrentTokens: 0,
+		}, err
 	} else {
 		log.Info().Msg("Accepting the request")
 	}
 
-	return true, nil
+	return &models.LimiterResponse{
+		Allowed:       allowed,
+		RetryAfter:    int64(retryAfter),
+		CurrentTokens: int64(currentTokens),
+	}, nil
 }
